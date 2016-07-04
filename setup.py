11cc780a6f933d4364e11ed5f3fcd3e1d0148bec
j0hnsmith <info@whywouldwe.com>
2016-07-04 16:19:00 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import dirname, join
 
 setup(
     name = "django-modelclone",
-    version = "0.5.3",
+    version = "0.5.4",
     description = u"Django application that allows users to clone a model in Admin",
     url = "https://github.com/RealGeeks/django-modelclone",
     packages = [
