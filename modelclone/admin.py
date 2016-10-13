df176ab9b00d1ca0dfedd623ff5100619e81bf25
David Krisch <david@hq.realgeeks.com>
2016-10-13 08:25:28 -1000

    sampleproject working with Django 1.8 & 1.9

--- modelclone/admin.py
+++ modelclone/admin.py
@@ -71,13 +71,6 @@ class ClonableModelAdmin(ModelAdmin):
         })
         return super(ClonableModelAdmin, self).change_view(request, object_id, form_url, extra_context)
 
-    def get_formsets(self, request):
-        try:
-            return super(ClonableModelAdmin, self).get_formsets(request)
-        except AttributeError:
-            # ModelAdmin.get_formsets has been removed in Django 1.9
-            return (f[0] for f in super(ClonableModelAdmin, self).get_formsets_with_inlines(request))
-
     def clone_view(self, request, object_id, form_url='', extra_context=None):
         opts = self.model._meta
 
@@ -95,9 +88,6 @@ class ClonableModelAdmin(ModelAdmin):
         ModelForm = self.get_form(request)
         formsets = []
 
-        # NOTE: Django 1.5 has a secong argument on get_inline_instances()
-        inline_instances = self.get_inline_instances(request)
-
         if request.method == 'POST':
             form = ModelForm(request.POST, request.FILES)
             if form.is_valid():
@@ -108,7 +98,7 @@ class ClonableModelAdmin(ModelAdmin):
                 form_validated = False
 
             prefixes = {}
-            for FormSet, inline in zip(self.get_formsets(request), inline_instances):
+            for FormSet, inline in self.get_formsets_with_inlines(request):
                 prefix = FormSet.get_default_prefix()
                 prefixes[prefix] = prefixes.get(prefix, 0) + 1
                 if prefixes[prefix] != 1 or not prefix:
@@ -136,17 +126,7 @@ class ClonableModelAdmin(ModelAdmin):
                     # In Django 1.9 we need one more param
                     self.log_addition(request, new_object, "Cloned object")
 
-                if VERSION[1] <= 4:
-                    # Until Django 1.4 giving %s in the url would be replaced with
-                    # object primary key.
-                    # I can't use the default because it goes back only one level
-                    # ('../%s/') and now we are under clone url, so we need one more level
-                    post_url_continue = '../../%s/'
-                else:
-                    # Since 1.5 '%s' was deprecated and if None is given reverse() will
-                    # be used and do the right thing
-                    post_url_continue = None
-                return self.response_add(request, new_object, post_url_continue)
+                return self.response_add(request, new_object, None)
 
         else:
             initial = model_to_dict(original_obj)
@@ -154,7 +134,7 @@ class ClonableModelAdmin(ModelAdmin):
             form = ModelForm(initial=initial)
 
             prefixes = {}
-            for FormSet, inline in zip(self.get_formsets(request), inline_instances):
+            for FormSet, inline in self.get_formsets_with_inlines(request):
                 prefix = FormSet.get_default_prefix()
                 prefixes[prefix] = prefixes.get(prefix, 0) + 1
                 if prefixes[prefix] != 1 or not prefix:
@@ -194,7 +174,7 @@ class ClonableModelAdmin(ModelAdmin):
         media = self.media + admin_form.media
 
         inline_admin_formsets = []
-        for inline, formset in zip(inline_instances, formsets):
+        for inline, formset in zip(self.get_inline_instances(request), formsets):
             fieldsets = list(inline.get_fieldsets(request, original_obj))
             readonly = list(inline.get_readonly_fields(request, original_obj))
             prepopulated = dict(inline.get_prepopulated_fields(request, original_obj))
---
+++ sampleproject/posts/migrations/0001_initial.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('author', models.CharField(max_length=256)),
+                ('content', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Multimedia',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=256)),
+                ('image', models.ImageField(upload_to=b'images', blank=True)),
+                ('document', models.FileField(upload_to=b'documents', blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=256)),
+                ('content', models.TextField(blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='tags',
+            field=models.ManyToManyField(to='posts.Tag', blank=True),
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='post',
+            field=models.ForeignKey(to='posts.Post'),
+        ),
+    ]
