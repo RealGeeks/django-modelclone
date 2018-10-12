7a712c7a1e75a2c662498632250ace30cb17106d
Igor Sobreira <igor@igorsobreira.com>
2018-10-11 16:24:26 -1000

    new version 0.7.0 supports python 3 and django 2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import dirname, join
 
 setup(
     name = "django-modelclone",
-    version = "0.6.1",
+    version = "0.7.0",
     description = u"Django application that allows users to clone a model in Admin",
     url = "https://github.com/RealGeeks/django-modelclone",
     packages = [
